a4051b931eb1602ab0fc664a3dec3a5ffa1d4336
Cindy Zhang <cindyxz@meta.com>
2026-03-27 07:22:52 -0700

    Revert "feat(sandbox): build status notification button in top nav (#922)" (#924)

--- apps/sandbox/src/app/SandboxTopNav.tsx
+++ apps/sandbox/src/app/SandboxTopNav.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import {XDSTopNav, XDSTopNavHeading} from '@xds/core/TopNav';
-import {BuildStatus} from './BuildStatus';
 
 export function SandboxTopNav() {
   return (
     <XDSTopNav
       label="Main navigation"
       heading={<XDSTopNavHeading heading="XDS Sandbox" />}
-      endContent={<BuildStatus />}
     />
   );
 }
